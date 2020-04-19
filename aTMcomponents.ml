03a2417a20fa859cadf72803701301e85e2c983f
Olivia Fu <olivia@Olivias-MacBook-Pro.local>
2020-04-19 15:01:16 -0700

    finished

--- aTMcomponents.ml
+++ aTMcomponents.ml
@@ -1,4 +1,4 @@
-type id = int
+(* type id = int
 
 type action =
   | Balance           (* balance inquiry *)
@@ -19,19 +19,90 @@ let acquire_id =
 	let id_list = List.map (fun x )
 	if List.mem id 
 
-
-
-
-
-
-
-(* initialize accts -- Establishes a database of accounts, each with a
-   name, aribtrary id, and balance. The names and balances are
-   initialized as per the `accts` provided. *)
-
-
 let present_message (message: string) : unit = 
 	print_string message ;;
 
 let deliver_cash (cash : int) : unit =
-	print_int cash ;;
+	print_int cash ;; *)
+
+open Printf ;;
+open Scanf ;;
+  
+(* Make use of an account database *)
+module DB = Database3 ;; 
+
+(* Customer account identifiers *)
+type id = int ;;
+
+(* Possible actions that an ATM customer can perform *)
+type action =
+  | Balance           (* balance inquiry *)
+  | Withdraw of int   (* withdraw an amount *)
+  | Deposit of int    (* deposit an amount *)
+  | Next              (* finish this customer and move on to the next one *)
+  | Finished          (* shut down the ATM and exit entirely *)
+;; 
+
+(* A specification of a customer name and initial balance *)
+type account_spec = {name : string; id : id; balance : int} ;;
+
+(*....................................................................
+  ATM component behaviors. See .mli file for further documentation.
+ *)
+
+let initialize (initial : account_spec list) : unit =
+  initial
+  |> List.iter (fun {name; id; balance}
+                -> DB.create id name;
+                   DB.update id balance) ;;
+
+let rec acquire_id () : id =
+  printf "Enter customer id: "; 
+  try
+    let id = read_int () in
+    ignore (DB.exists id); id
+  with
+  | Not_found 
+  | Failure _ -> printf "Invalid id \n";
+                 acquire_id () ;;
+                  
+let rec acquire_amount () : int =
+  printf "Enter amount: ";
+  try
+    let amount = read_int () in
+    if amount <= 0 then raise (Failure "amount is non-positive");
+    amount
+  with
+  | Failure _ -> printf "Invalid amount \n";
+                 acquire_amount () ;;
+  
+let rec acquire_act () : action =
+  printf "Enter action: (B) Balance (-) Withdraw (+) Deposit \
+          (=) Done (X) Exit: %!";
+  scanf " %c"
+        (fun char -> match char with
+                     | 'b' | 'B'        -> Balance
+                     | '/' | 'x' | 'X'  -> Finished
+                     | '='              -> Next
+                     | 'w' | 'W' | '-'  -> Withdraw (acquire_amount ())
+                     | 'd' | 'D' | '+'  -> Deposit (acquire_amount ())
+                     | _                -> printf "  invalid choice\n";
+                                           acquire_act () ) ;;
+
+let get_balance : id -> int = DB.balance ;;
+
+let get_name : id -> string = DB.name ;;
+
+let update_balance : id -> int -> unit = DB.update ;;
+
+let present_message (msg : string) : unit = 
+  printf "%s\n%!" msg ;;
+  
+let deliver_cash (amount : int) : unit =
+  printf "Here's your cash: ";
+  (* dispense some "20's" *)
+  for _i = 1 to (amount / 20) do
+    printf "[20 @ 20]"
+  done;
+  (* dispense the rest of the cash *)
+  printf " and %d more\n" (amount mod 20) ;;
